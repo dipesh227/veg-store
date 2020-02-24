4413e36c4a4e1ee371d1b8908d376f3690042195
dipesh joshi <dipeshjoshi227@gmail.com>
2020-02-24 19:23:30 +0530

    update

--- header.php
+++ header.php
@@ -6,17 +6,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vag Store</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header class="header">
         <div id="left" class="head">
-            ifk
+            <img src="img/logo" alt="" class="">
         </div>
 
         <div id="center" class="head">
-            kjj
+            <ul class="navbar">
+                <li><a href="">home</a></li>
+                <li><a href="">about us</a></li>
+                <li><a href="">contectus</a></li>
+                <li><a href="">services</a></li>
+                <li><a href="">shop kiper</a></li>
+            </ul>
         </div>
 
         <div id="right" class="head">

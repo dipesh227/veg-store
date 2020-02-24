d7bf9819a863448773f3949a6ca01eec109b27db
dipesh joshi <dipeshjoshi227@gmail.com>
2020-02-24 16:41:21 +0530

    dip

--- header.php
+++ header.php
@@ -1,11 +1,16 @@
+<?php include('connection.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vag Store</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+
 </body>
+
 </html>

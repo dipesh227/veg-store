f957dc14803fefb03250dc5f43a50b91be0b9350
dipesh joshi <dipeshjoshi227@gmail.com>
2020-02-24 17:35:28 +0530

    ypdate

--- header.php
+++ header.php
@@ -6,11 +6,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vag Store</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+    <header class="header">
+        <div id="left" class="head">
+ifk
+        </div>
 
+        <div id="center" class="head">
+kjj
+        </div>
+
+        <div id="right" class="head">
+joojs
+        </div>
+    </header>
 </body>
 
 </html>

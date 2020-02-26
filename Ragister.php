9105e04b4bed732e839745e95689999976395ca1
dipesh joshi <dipeshjoshi227@gmail.com>
2020-02-26 14:26:41 +0530

    style update

--- Ragister.php
+++ Ragister.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>flex box</title>
-    <style>
+
+    <!-- <style>
+        {
     .container {
         height: 400px;
         width: 100%;
@@ -14,17 +16,14 @@
         /* flex-direction: row-reverse; */
         /* flex-wrap: wrap; */
         /* flex-wrap: wrap-reverse; */
-        flex-flow: row-reverse wrap;
+        /* flex-flow: row-reverse wrap; */
         /* justify-content: center;
-            justify-content: space-between; */
-        justify-content: space-evenly;
+        justify-content: space-between; */
+        /* justify-content: space-evenly;
         justify-content: space-around;
         align-items: center;
-        align-items: stretch;
+        align-items: stretch; */
         /* align-items: flex-end; */
-
-
-
     }
 
     .item {
@@ -34,15 +33,108 @@
         border: 2px solid green;
         margin: 10px;
         padding: 3px;
+    }
+
+    #item1 {
+        /* order: 2; */
+        /* flex-grow: 3;
+        flex-shrink: 2; */
 
     }
-    </style>
+
+    #item2 {
+        /* flex-grow: 2;
+        flex-shrink: 3; */
+        /* flex-basis: px; */
+    }
+
+    #item3 {
+        /* order: 40; */
+        /* align-self: flex-end;
+        flex: 2 2 22px; */
+    }
+    </style> -->
+    <!-- <style>
+    html {
+        font-size: 25px;
+        
+    }
+
+    .container {
+        padding: 10em;
+        margin: 10em;
+        /* width: 100vw; */
+        height: 100%;
+         
+        font-size: 10px;
+        border: 2px solid black;
+        border-radius:100%;
+        }
+
+    h1 {
+        /* text-align: center; */
+        /* font-size: 10em; */
+    }
+
+    #frist {
+        /* font-size: 3em;
+            padding: 3em; */
+    }
+
+    #second {
+        /* font-size: 3rem; */
+    }
+    </style> }-->
+    <!-- <style>
+        .box{
+            background-color: red;
+            font-size: 50px;
+            text-align: center;
+            color: white;
+            display: none;
+        }
+        @media (max-width:300px){
+            #box1{
+                display: block;
+            }
+        }
+        @media only screen and (max-width:500px) and (min-width:300px){
+            #box2{
+                display: block;
+            }
+        }
+        @media (max-width:800px) and (min-width:500px){
+            #box3{
+                display: block;
+            }
+        }
+        @media (min-width:800px){
+            #box4{
+                display: block;
+            }
+        }
+
+    </style> -->
+
+
+
 </head>
 
+
 <body>
-    <h1>this is flexbox</h1>
-    <div class="container">
-        <div class="item" id="item1">
+
+
+
+    <!-- <div class="box" id="box1">box1</div>
+        <div class="box" id="box2">box2</div>
+        <div class="box" id="box3">box3</div>
+        <div class="box" id="box4">box4</div> -->
+    <!-- <div class="container">
+        <h1 id="frist">this is flexbox</h1>
+        <h1 id="second">this is flexbox</h1>
+        <h1 id="third">this is flexbox</h1>
+    </div> -->
+    <!-- <div class="item" id="item1">
             first box
         </div>
 
@@ -54,7 +146,7 @@
             first3 box
         </div>
 
-        <div class="item" id="item4">
+       <div class="item" id="item4">
             first4 box
         </div>
 
@@ -64,8 +156,8 @@
 
         <div class="item" id="item6">
             first6 box
-        </div>
-        </di v>
+        </div> 
+    </div> -->
 </body>
 
 </html>
